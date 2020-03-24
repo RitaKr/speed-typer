1a81459a425d56ecc4299ab99ea6a23ba76c96d8
RitaKr <ritokra@gmail.com>
2020-03-24 19:20:46 +0200

    all

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const scoreEl = document.getElementById('score');
 const timeEl = document.getElementById('time');
 const endgameEl = document.getElementById('end-game');
 const settingsForm = document.getElementById('settings-form');
+const difficultySelect = document.getElementById('difficulty');
+
 const words = [
     'apple',
     'home',
@@ -20,7 +22,15 @@ const words = [
 let randomWord;
 let score = 0;
 let time = 10;
+let difficulty = 
+sessionStorage.getItem('difficulty') !== null 
+? sessionStorage.getItem('difficulty')
+: 'medium';
 
+difficultySelect.value = 
+sessionStorage.getItem('difficulty') !== null 
+? sessionStorage.getItem('difficulty')
+: 'medium';
 text.focus();
 const timeInterval = setInterval(updateTime, 1000);
 
@@ -37,7 +47,7 @@ function gameOver(){
     endgameEl.innerHTML = `
     <h1>Game over!</h1>
     <p>Your score is ${score}</p>
-    <button onclick="location.reload()">Reload</button>`;
+    <button onclick="location.reload()" id="restart">Reload</button>`;
     endgameEl.style.display = 'flex';
 }
 
@@ -64,7 +74,30 @@ text.addEventListener('input',e =>{
         addWordToDOM();
         updateScore();
         e.target.value='';
+        switch (difficulty) {
+            case 'hard': time+=2;
+            break;
+            case 'middle': time+=4;
+            break;
+            case 'easy': time+=6;
+            break;
+        }
+        
+       /*if (difficultySelect==='hard') {
+        time+=2;
+       }
+       if (difficultySelect==='middle') {
         time+=4;
+       } else {
+        time+=6;
+       }
+       */
         updateTime();
     }
+})
+settingsForm.addEventListener('change', (e)=>{
+    console.log(e.target.value);
+    difficulty = e.target.value;
+    sessionStorage.setItem('difficulty', difficulty);
+    text.focus();
 })
